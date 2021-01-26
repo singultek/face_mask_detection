41cc3ceb8b28c45af0032266721eb3ee2312ce67
singultek <sinan.gultekin95@gmail.com>
2021-01-26 15:58:56 +0100

    Adding Code Skeleton

---
+++ packages/network.py
@@ -0,0 +1,57 @@
+""" Copyright 2021 - Sinan Gültekin <singultek@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+class CNNClassifier(nn.Module):
+    '''
+    Class
+    '''
+    def __init__(self):
+
+        #if Resnet
+
+        #elif Basic_CNN
+
+        #else None
+
+    def save(self):
+        return
+
+    def load(self):
+        return
+
+    def forward(self):
+        return
+
+    @staticmethod
+    def __decision(self):
+        return
+
+    def train_model(self):
+        return
+
+    def eval_model(self):
+        return
+
+    def classify_input(self):
+        return
+
+    @staticmethod
+    def __loss(self):
+        return
+
+    @staticmethod
+    def __performance(self):
+        return
+
+    def __plot(self):
+        return
